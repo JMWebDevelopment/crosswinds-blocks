7cee18c6e79be9bb82f95ea0395782ab0ee9ba97
Jacob Martella <me@jacobmartella.com>
2023-03-23 21:53:38 -0500

    Continued working on social sharing content block.
    Addresses #10

--- blocks/build/social-share-content/index.asset.php
+++ blocks/build/social-share-content/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f67c923cb490b79e81b6');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ecbb85021df648dc1154');
--- blocks/src/social-share-content/edit.js
+++ blocks/src/social-share-content/edit.js
@@ -25,10 +25,20 @@ import {
 * External dependencies
 */
 import { MultiSelectControl } from '@codeamp/block-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import {
+	faFacebookF,
+	faTwitter,
+	faMastodon,
+	faReddit,
+	faPinterest
+} from '@fortawesome/free-brands-svg-icons';
 
 /**
  * Internal dependencies
  */
+import { socialIconsSVG } from './icons';
 import './editor.scss';
 
 export function Edit( props ) {
@@ -118,6 +128,39 @@ export function Edit( props ) {
 		{
 			value: 'auto-width',
 			label: __( 'Auto Width', 'crosswinds-blocks' ),
+		},
+	];
+
+	const socialSites = [
+		{
+			site: 'facebook',
+			label: __( 'Facebook', 'crosswinds-blocks' ),
+			icon: faFacebookF,
+		},
+		{
+			site: 'twitter',
+			label: __( 'Twitter', 'crosswinds-blocks' ),
+			icon: faTwitter,
+		},
+		{
+			site: 'mastodon',
+			label: __( 'Mastodon', 'crosswinds-blocks' ),
+			icon: faMastodon,
+		},
+		{
+			site: 'reddit',
+			label: __( 'Reddit', 'crosswinds-blocks' ),
+			icon: faReddit,
+		},
+		{
+			site: 'pinterest',
+			label: __( 'Pinterest', 'crosswinds-blocks' ),
+			icon: faPinterest,
+		},
+		{
+			site: 'email',
+			label: __( 'Email', 'crosswinds-blocks' ),
+			icon: faEnvelope,
 		},
 	];
 
@@ -230,6 +273,30 @@ export function Edit( props ) {
 		</>
 	);
 
+	const socialIconsDisplay = socialSites.map( ( site ) => {
+		if ( socialIcons.includes( site.site ) ) {
+			if ( 'show-label-icon' === iconStyle ) {
+				return (
+					<div className={ 'social-icon' }>
+						<span><FontAwesomeIcon icon={ site.icon } /> { site.label }</span>
+					</div>
+				);
+			} else if ( 'show-icon' === iconStyle ) {
+				return (
+					<div className={ 'social-icon' }>
+						<span><FontAwesomeIcon icon={ site.icon } /></span>
+					</div>
+				);
+			} else {
+				return (
+					<div className={ 'social-icon' }>
+						<span>{ site.label }</span>
+					</div>
+				);
+			}
+		}
+	} );
+
 	/*const sliderBackgroundStyles = {
 		background: gradientValue,
 		backgroundColor: sliderBackgroundColorValue,
@@ -250,15 +317,17 @@ export function Edit( props ) {
 		<>
 			{ inspectorControls }
 			<div { ...useBlockProps() }>
-
+				{ socialIconsDisplay }
 			</div>
 		</>
 	);
 }
 
 const sliderColorAttributes = {
-	sliderColor: 'slider-color',
-	sliderBackgroundColor: 'slider-background-color',
+	iconColor: 'icon-color',
+	iconBackgroundColor: 'icon-background-color',
+	iconHoverColor: 'icon-hovercolor',
+	iconHoverBackgroundColor: 'icon-hover-background-color',
 };
 
 export default withColors( sliderColorAttributes )( Edit );
