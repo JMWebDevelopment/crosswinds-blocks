ff1b032a4437a7de0ed236b8fed0dfdc1b0334f5
Jacob Martella <me@jacobmartella.com>
2023-04-03 21:11:22 -0500

    Added in the default and sidebar page templates.
    Addresses #6 #9

--- blocks/build/breadcrumbs/index.asset.php
+++ blocks/build/breadcrumbs/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ca048fa26682662ba697');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'cd42984bf65c05e3b63c');
