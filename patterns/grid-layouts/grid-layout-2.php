22905c445ac9b2ca1c6f35258f7a4a4b4a541c0e
Jacob Martella <me@jacobmartella.com>
2023-04-19 09:30:19 -0500

    Added in three basic grid layout patterns.
    Addresses #39

---
+++ patterns/grid-layouts/grid-layout-2.php
@@ -0,0 +1,56 @@
+<?php
+
+return array(
+	'title'         => esc_html__( 'Grid Layout 2', 'crosswinds-blocks' ),
+	'categories'    => array( 'crosswinds-blocks' ),
+	'viewportWidth' => 1200,
+	'content'       => '<!-- wp:crosswinds-blocks/basic-grid {"numDesktopColumns":3,"numTabletColumns":3,"align":"wide","className":"grid-layout-2"} -->
+						<div class="wp-block-crosswinds-blocks-basic-grid alignwide basic-grid-has-3-desktop-columns basic-grid-has-3-tablet-columns basic-grid-has-1-mobile-columns grid-layout-2" style="grid-column-gap:30px;grid-row-gap:30px"><!-- wp:crosswinds-blocks/basic-grid-item {"gridColumnSpanDesktop":3,"gridColumnSpanTablet":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|tertiary"}}},"spacing":{"padding":{"top":"var:preset|spacing|x-small","right":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|x-small"}}},"backgroundColor":"contrast","textColor":"base"} -->
+						<div class="wp-block-crosswinds-blocks-basic-grid-item item-span-3-desktop-columns item-span-1-desktop-row item-span-3-tablet-columns item-span-1-tablet-row item-span-1-mobile-columns item-span-1-mobile-row normal-layout has-base-color has-contrast-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--x-small);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--x-small)"><!-- wp:group {"cbUseFlex":true,"cbInnerLayout":"normal","cbFillHeight":true} -->
+						<div class="wp-block-group normal-layout cb-fill-height-parent"><!-- wp:heading {"placeholder":"' . __( 'Insert your heading here...', 'crosswinds-blocks' ) . '"} -->
+						<h2 class="wp-block-heading"></h2>
+						<!-- /wp:heading -->
+						
+						<!-- wp:paragraph {"placeholder":"' . __( 'Write some description text here...', 'crosswinds-blocks' ) . '"} -->
+						<p></p>
+						<!-- /wp:paragraph --></div>
+						<!-- /wp:group --></div>
+						<!-- /wp:crosswinds-blocks/basic-grid-item -->
+						
+						<!-- wp:crosswinds-blocks/basic-grid-item {"style":{"elements":{"link":{"color":{"text":"var:preset|color|tertiary"}}},"spacing":{"padding":{"top":"var:preset|spacing|x-small","right":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|x-small"}}},"backgroundColor":"contrast","textColor":"base"} -->
+						<div class="wp-block-crosswinds-blocks-basic-grid-item item-span-1-desktop-columns item-span-1-desktop-row item-span-1-tablet-columns item-span-1-tablet-row item-span-1-mobile-columns item-span-1-mobile-row normal-layout has-base-color has-contrast-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--x-small);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--x-small)"><!-- wp:group {"cbUseFlex":true,"cbInnerLayout":"normal","cbFillHeight":true} -->
+						<div class="wp-block-group normal-layout cb-fill-height-parent"><!-- wp:heading {"placeholder":"' . __( 'Insert your heading here...', 'crosswinds-blocks' ) . '"} -->
+						<h2 class="wp-block-heading"></h2>
+						<!-- /wp:heading -->
+						
+						<!-- wp:paragraph {"placeholder":"' . __( 'Write some description text here...', 'crosswinds-blocks' ) . '"} -->
+						<p></p>
+						<!-- /wp:paragraph --></div>
+						<!-- /wp:group --></div>
+						<!-- /wp:crosswinds-blocks/basic-grid-item -->
+						
+						<!-- wp:crosswinds-blocks/basic-grid-item {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","right":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|x-small"}},"elements":{"link":{"color":{"text":"var:preset|color|tertiary"}}}},"backgroundColor":"contrast","textColor":"base"} -->
+						<div class="wp-block-crosswinds-blocks-basic-grid-item item-span-1-desktop-columns item-span-1-desktop-row item-span-1-tablet-columns item-span-1-tablet-row item-span-1-mobile-columns item-span-1-mobile-row normal-layout has-base-color has-contrast-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--x-small);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--x-small)"><!-- wp:group {"cbUseFlex":true,"cbInnerLayout":"normal","cbFillHeight":true} -->
+						<div class="wp-block-group normal-layout cb-fill-height-parent"><!-- wp:heading {"placeholder":"' . __( 'Insert your heading here...', 'crosswinds-blocks' ) . '"} -->
+						<h2 class="wp-block-heading"></h2>
+						<!-- /wp:heading -->
+						
+						<!-- wp:paragraph {"placeholder":"' . __( 'Write some description text here...', 'crosswinds-blocks' ) . '"} -->
+						<p></p>
+						<!-- /wp:paragraph --></div>
+						<!-- /wp:group --></div>
+						<!-- /wp:crosswinds-blocks/basic-grid-item -->
+						
+						<!-- wp:crosswinds-blocks/basic-grid-item {"style":{"elements":{"link":{"color":{"text":"var:preset|color|tertiary"}}}},"backgroundColor":"contrast","textColor":"base"} -->
+						<div class="wp-block-crosswinds-blocks-basic-grid-item item-span-1-desktop-columns item-span-1-desktop-row item-span-1-tablet-columns item-span-1-tablet-row item-span-1-mobile-columns item-span-1-mobile-row normal-layout has-base-color has-contrast-background-color has-text-color has-background has-link-color"><!-- wp:group {"cbUseFlex":true,"cbInnerLayout":"normal","cbFillHeight":true} -->
+						<div class="wp-block-group normal-layout cb-fill-height-parent"><!-- wp:heading {"placeholder":"' . __( 'Insert your heading here...', 'crosswinds-blocks' ) . '"} -->
+						<h2 class="wp-block-heading"></h2>
+						<!-- /wp:heading -->
+						
+						<!-- wp:paragraph {"placeholder":"' . __( 'Write some description text here...', 'crosswinds-blocks' ) . '"} -->
+						<p></p>
+						<!-- /wp:paragraph --></div>
+						<!-- /wp:group --></div>
+						<!-- /wp:crosswinds-blocks/basic-grid-item --></div>
+						<!-- /wp:crosswinds-blocks/basic-grid -->',
+);
