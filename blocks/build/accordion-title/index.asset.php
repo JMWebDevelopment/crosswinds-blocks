2187d0ae2222ff911199e41b86b44d1727e83bf6
Jacob Martella <me@jacobmartella.com>
2023-05-11 21:48:50 -0500

    Fixed up the accordion blocks.

--- blocks/build/accordion-title/index.asset.php
+++ blocks/build/accordion-title/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'c104cca83f686bbfc11c');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'd46dba37484d6d221470');
--- blocks/build/accordion/index.asset.php
+++ blocks/build/accordion/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '34700da4d57420aa5f01');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2fca69239c30305a7a9d');
