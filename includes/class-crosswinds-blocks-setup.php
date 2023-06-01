a774f16372c5f78d68a2e36c7cf8f8c820bd02f9
Jacob Martella <me@jacobmartella.com>
2023-05-31 20:16:49 -0500

    Changed the portfolio CPT to projects.

--- includes/class-crosswinds-blocks-setup.php
+++ includes/class-crosswinds-blocks-setup.php
@@ -27,37 +27,37 @@ class Crosswinds_Blocks_Setup {
 	 */
 	public function create_custom_post_types() {
 
-		// Add the portfolio custom post type.
-		if ( get_option( 'crosswinds-blocks-portfolio-cpt' ) || apply_filters( 'crosswinds_blocks_enable_portfolio_cpt', false ) ) {
+		// Add the project custom post type.
+		if ( get_option( 'crosswinds-blocks-project-cpt' ) || apply_filters( 'crosswinds_blocks_enable_project_cpt', false ) ) {
 			register_post_type(
-				'portfolio',
+				'project',
 				array(
 					'labels'       => array(
-						'name'               => esc_html__( 'Portfolio', 'crosswinds-blocks' ),
-						'singular_name'      => esc_html__( 'Portfolio Project', 'crosswinds-blocks' ),
-						'add_new'            => _x( 'Add New', 'Portfolio Project', 'crosswinds-blocks' ),
-						'add_new_item'       => esc_html__( 'Add New Portfolio Project', 'crosswinds-blocks' ),
+						'name'               => esc_html__( 'Projects', 'crosswinds-blocks' ),
+						'singular_name'      => esc_html__( 'Project', 'crosswinds-blocks' ),
+						'add_new'            => _x( 'Add New', 'Project', 'crosswinds-blocks' ),
+						'add_new_item'       => esc_html__( 'Add New Project', 'crosswinds-blocks' ),
 						'edit_item'          => esc_html__( 'Edit Item', 'crosswinds-blocks' ),
 						'new_item'           => esc_html__( 'New Item', 'crosswinds-blocks' ),
 						'view_item'          => esc_html__( 'View Item', 'crosswinds-blocks' ),
-						'search_items'       => esc_html__( 'Search Portfolio Project', 'crosswinds-blocks' ),
-						'not_found'          => esc_html__( 'No Portfolio Projects found', 'crosswinds-blocks' ),
-						'not_found_in_trash' => esc_html__( 'No Portfolio Projects found in Trash', 'crosswinds-blocks' ),
+						'search_items'       => esc_html__( 'Search Project', 'crosswinds-blocks' ),
+						'not_found'          => esc_html__( 'No Projects found', 'crosswinds-blocks' ),
+						'not_found_in_trash' => esc_html__( 'No Projects found in Trash', 'crosswinds-blocks' ),
 						'parent_item_colon'  => '',
 					),
 					'public'       => true,
 					'has_archive'  => false,
 					'supports'     => array( 'title', 'editor', 'excerpt', 'thumbnail', 'revisions', 'comments', 'custom-fields' ),
 					'query_var'    => true,
-					'rewrite'      => array( 'slug' => 'porftolio' ),
+					'rewrite'      => array( 'slug' => 'project' ),
 					'menu_icon'    => 'dashicons-portfolio',
 					'show_in_rest' => true,
 				)
 			);
 
 			register_post_meta(
-				'portfolio',
-				'portafoglio_client',
+				'project',
+				'project_client',
 				array(
 					'show_in_rest' => true,
 					'single'       => true,
@@ -66,8 +66,8 @@ class Crosswinds_Blocks_Setup {
 			);
 
 			register_post_meta(
-				'portfolio',
-				'portafoglio_date',
+				'project',
+				'project_date',
 				array(
 					'show_in_rest' => true,
 					'single'       => true,
@@ -85,16 +85,16 @@ class Crosswinds_Blocks_Setup {
 	 */
 	public function create_custom_taxonomies() {
 
-		// Register the portfolio category taxonomy.
-		if ( get_option( 'crosswinds-blocks-portfolio-category-taxonomy' ) || apply_filters( 'crosswinds_blocks_enable_portfolio-category_taxonomy', false ) ) {
+		// Register the project category taxonomy.
+		if ( get_option( 'crosswinds-blocks-project-cpt' ) || apply_filters( 'crosswinds_blocks_enable_project_cpt', false ) ) {
 			register_taxonomy(
-				'portfolio_category',
-				'portfolio',
+				'project_category',
+				'project',
 				array(
 					'hierarchical'       => true,
 					'labels'             => array(
-						'name'                  => esc_html__( 'Portfolio Categories', 'crosswinds-blocks' ),
-						'singular_name'         => esc_html__( 'Portfolio Category', 'crosswinds-blocks' ),
+						'name'                  => esc_html__( 'Project Categories', 'crosswinds-blocks' ),
+						'singular_name'         => esc_html__( 'Project Category', 'crosswinds-blocks' ),
 						'search_items'          => esc_html__( 'Search Categories', 'crosswinds-blocks' ),
 						'all_items'             => esc_html__( 'All Categories', 'crosswinds-blocks' ),
 						'parent_item'           => esc_html__( 'Parent Category', 'crosswinds-blocks' ),
@@ -109,23 +109,20 @@ class Crosswinds_Blocks_Setup {
 					'show_ui'            => true,
 					'show_admin_column'  => true,
 					'query_var'          => true,
-					'rewrite'            => array( 'slug' => 'portfolio-category' ),
+					'rewrite'            => array( 'slug' => 'project-category' ),
 					'show_in_rest'       => true,
 					'publicly_queryable' => true,
 				)
 			);
-		}
-
-		// Register the portfolio category taxonomy.
-		if ( get_option( 'crosswinds-blocks-portfolio-tag-taxonomy' ) || apply_filters( 'crosswinds_blocks_enable_portfolio-tag_taxonomy', false ) ) {
+		
 			register_taxonomy(
-				'portfolio_tag',
-				'portfolio',
+				'project_tag',
+				'project',
 				array(
 					'hierarchical'      => false,
 					'labels'            => array(
-						'name'                  => esc_html__( 'Portfolio Tags', 'crosswinds-blocks' ),
-						'singular_name'         => esc_html__( 'Portfolio Tag', 'crosswinds-blocks' ),
+						'name'                  => esc_html__( 'Project Tags', 'crosswinds-blocks' ),
+						'singular_name'         => esc_html__( 'Project Tag', 'crosswinds-blocks' ),
 						'search_items'          => esc_html__( 'Search Tags', 'crosswinds-blocks' ),
 						'all_items'             => esc_html__( 'All Tags', 'crosswinds-blocks' ),
 						'parent_item'           => esc_html__( 'Parent Tag', 'crosswinds-blocks' ),
@@ -140,8 +137,10 @@ class Crosswinds_Blocks_Setup {
 					'show_ui'           => true,
 					'show_admin_column' => true,
 					'query_var'         => true,
-					'rewrite'           => array( 'slug' => 'portfolio-tag' ),
+					'rewrite'           => array( 'slug' => 'project-tag' ),
 					'show_in_rest'      => true,
+					'show_in_variation' => true,
+					'publicly_queryable' => true,
 				)
 			);
 		}
