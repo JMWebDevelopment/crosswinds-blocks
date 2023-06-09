7b13468eec7d80aaef4a8de56416e65b7f5f5467
Jacob Martella <me@jacobmartella.com>
2023-06-08 19:38:32 -0500

    Added in final touches for EDD filters blocks.
    Addresses #48

--- admin/partials/plugin-settings.php
+++ admin/partials/plugin-settings.php
@@ -45,7 +45,7 @@ $blocks = array(
 		'name'        => esc_html__( 'Downloads Search Filters', 'crosswinds-blocks' ),
 		'description' => esc_html__( 'Adds in a set of download search blocks that allow you to create a download search filters section.', 'crosswinds-blocks' ),
 		'documentation_link' => '',
-		'icon'        => '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="#1b1930" class="fa-primary" d="M572.6 270.3l-96 192C471.2 473.2 460.1 480 447.1 480H0l119.2-238.3C124.6 230.8 135.7 224 147.8 224h396.2C567.7 224 583.2 249 572.6 270.3z"/><path fill="#3e7ea8" class="fa-secondary" d="M480 144V224H147.8C135.7 224 124.6 230.8 119.2 241.7L0 480V80C0 53.49 21.49 32 48 32h160l64 64h160C458.5 96 480 117.5 480 144z"/></svg>',
+		'icon'        => '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path fill="#1b1930" class="fa-primary" d="M256 0c17.7 0 32 14.3 32 32V306.7l73.4-73.4c12.5-12.5 32.8-12.5 45.3 0s12.5 32.8 0 45.3l-128 128c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L224 306.7V32c0-17.7 14.3-32 32-32z"/><path fill="#3e7ea8" class="fa-secondary" d="M178.7 352H64c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H333.3l-54.6 54.6c-12.5 12.5-32.8 12.5-45.3 0L178.7 352zM408 432a24 24 0 1 1 48 0 24 24 0 1 1 -48 0z"/></svg>',
 	),
 	'google-maps' => array(
 		'slug'        => 'google-maps',
